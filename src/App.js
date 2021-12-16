6038f50292ab0c50dff84137f040c06b960b3250
Amrutha Joshi <amruthajoshi33@gmail.com>
2021-12-16 02:14:07 -0600

    added hash router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import LoginPage from './components/LoginPage';
 import Videos from './components/Videos';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, HashRouter as Router, Route, Switch } from "react-router-dom";
 import SignUp from './components/SignUpPage';
 import AppManager from './components/AppManager';
 import Groups from './components/Groups';
@@ -13,32 +13,32 @@ function App() {
   return (
     <div className="App">
       <AppManager />
-      <BrowserRouter basename={'/dsd4'}>
+      <Router >
         <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/dashboard`}>
+          <Route exact path="/dashboard">
             <Dashboard />
           </Route>
-          <Route exact path={`${process.env.PUBLIC_URL}/signup`}>
+          <Route exact path="/signup">
             <SignUp />
           </Route>
-          <Route exact path={`${process.env.PUBLIC_URL}/`}>
+          <Route exact path="/">
             <LoginPage />
           </Route>
-          <Route exact path={`${process.env.PUBLIC_URL}/video`}>
+          <Route exact path="/video">
             <Videos />
           </Route>
-          <Route exact path={`${process.env.PUBLIC_URL}/groups`}>
+          <Route exact path="/groups">
             <Groups />
           </Route>
-          <Route exact path={`${process.env.PUBLIC_URL}/group/:id`}>
+          <Route exact path="/group/:id">
             <GroupDetails />
           </Route>
-          <Route exact path={`${process.env.PUBLIC_URL}/problem-statements`}>
+          <Route exact path="/problem-statements">
             <ProblemStatements />
           </Route>
          
         </Switch>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
